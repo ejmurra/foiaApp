9cb4168969925b44a833ba15abf274c1fb9f0c92
Eli Murray <ejmurra2@illinois.edu>
2014-11-09 17:18:30 -0600

    Basic api and front end

--- models/request.js
+++ models/request.js
@@ -1,5 +1,13 @@
 var db = require('../db')
 var Request = db.model('Request', {
-  name: {type: String, required: true },
-  request:
+  parentUser: {type: String, required: true},
+  date: {type: Date, default: Date.now},
+  response: {type: Boolean, default: false},
+  to: {type: String, required: true},
+  subject: {type: String, required: true},
+  text: {type: String, required: true},
+  html: {type: String},
+  responseText: {type: String},
+  attachment: {type: String}
 })
+module.exports = Request
--- server.js
+++ server.js
@@ -1,27 +1,33 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Request = require('./models/request')
 
 var app = express()
 app.use(bodyParser.json())
 
-app.get('/api/requests', function (req, res) {
-  res.json([
-    {
-      name: 'Eli Murray',
-      request: 'Link 2 Obj',
-      response: false,
-      timestamp: '10:45 am'
-    }
-  ])
+app.get('/api/requests', function (req, res, next) {
+  Request.find(function(err, requests) {
+    if (err) { return next(err) }
+    res.status(201).json(requests)
+  })
 })
 
-app.post('/api/requests', function (req, res) {
-  console.log('request received')
-  console.log(req.body.name)
-  console.log(req.body.request)
-  console.log(req.body.response)
-  console.log(req.body.timestamp)
-  res.sendStatus(201)
+app.post('/api/requests', function (req, res, next) {
+  var request = new Request({
+    parentUser: 'dickeyxxx', //req.user.username
+    response: false,
+    to: req.body.to,
+    subject: req.body.subject,
+    text: req.body.text,
+    html: null,
+    responseText: null,
+    attachment: null
+  })
+  request.save(function (err, request) {
+    if (err) { return next(err) }
+    console.log('saved')
+    console.log(parentUser)
+  })
 })
 
 app.listen(3000, function () {
