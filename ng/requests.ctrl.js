44dcacdda3d3dae627f9cf34346bd977dfaec4bb
Eli Murray <ejmurra2@illinois.edu>
2015-01-21 20:17:07 -0600

    Fixed scoping error

--- ng/requests.ctrl.js
+++ ng/requests.ctrl.js
@@ -1,17 +1,19 @@
 angular.module('app')
 .controller('RequestsCtrl', function($scope, RequestsSvc) {
+  $scope.request = {};
   $scope.makeRequest = function () {
-    if ($scope.requestText) {
+    if ($scope.request.text && $scope.request.subject && $scope.request.email) {
+      console.log('filed')
       RequestsSvc.create({
         username: $scope.currentUser.username,
-        subject: $scope.requestSubject,
-        toEmail: $scope.requestEmail,
+        subject: $scope.request.subject,
+        toEmail: $scope.request.email,
         //date: 'date.now',
         replied: false,
         requestFull: "My name is " + $scope.currentUser.username +
         " and I am a " + $scope.currentUser.position + " at " + $scope.currentUser.organization + ". " +
         "I am requesting the following information under the " + $scope.currentUser.state + " Freedom of Information Act: \n \n" +
-        $scope.requestText + "\n \n" +
+        $scope.request.text + "\n \n" +
         "I ask that you waive any and all fees associated with the gathering of this information as I am collecting and reporting on this information in the public interest. \n I also ask that you cite reasons for any redactions pursuant to Illinois FOIA law. \n My contact info is as follows: \n" +
         $scope.currentUser.username + "\n" +
         $scope.currentUser.addressLineOne + "\n" +
@@ -23,10 +25,12 @@ angular.module('app')
         response: 'There is no response yet.',
         attachment: 'There is no attachment.'
       }).success(function (request) {
-        $scope.requestText = null,
-        $scope.requestSubject = null,
-        $scope.requestEmail = null
+        $scope.request.text = null,
+        $scope.request.subject = null,
+        $scope.request.email = null
       })
+    } else {
+      console.log('request not sent', $scope.request.text, $scope.request.email, $scope.request.subject)
     }
   }
   RequestsSvc.fetch().success(function (requests) {
